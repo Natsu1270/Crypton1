0fa8f788052ae97ecdd7727155cad5b8907bcea2
cgz104 <cuong.le1004@gmail.com>
2019-03-23 11:11:57 +0700

    openfile

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace Crypton1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string strfilename = openFileDialog1.FileName;
+                MessageBox.Show(strfilename);
+            }
+        }
     }
 }
